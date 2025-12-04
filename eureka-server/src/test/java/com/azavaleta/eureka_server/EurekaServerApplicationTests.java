ab007cd87d46d54d8703a4442e27fa634101200e
anthony <anthony_zavaleta@camaracomercioexterior.edu.p>
2025-12-04 15:18:11 -0500

    config eureka-server

---
+++ eureka-server/src/test/java/com/azavaleta/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azavaleta.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
